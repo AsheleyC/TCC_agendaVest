06ea5bafa23e2c439a3e84e5b89e2f838cc59f1f
Asheley Gomes <asheleycgt@gmail.com>
2026-03-05 13:54:23 -0300

    Rota Atualizar email

--- back/server.js
+++ back/server.js
@@ -12,42 +12,42 @@ const porta = process.env.porta
 server.use(express.json())
 server.use(cors())
 
-server.listen(porta,()=>{  
-    console.log(`Servidor rodando em: http://localhost:${porta}`)  
+server.listen(porta, () => {
+    console.log(`Servidor rodando em: http://localhost:${porta}`)
 })
 
 // Visualizar Perfil
-server.get('/ver_perfil', async (req,res) =>{
+server.get('/ver_perfil', async (req, res) => {
     try {
         const [resultado] = await pool.query(`select nome_usuario, email, senha, foto_perfil from usuarios`)
-        res.json({"resposta":resultado})
+        res.json({ "resposta": resultado })
     } catch (error) {
         console.log(error)
-    }    
+    }
 })
 
 // Cadastrar Usuário
-server.post('/cadastro', async (req,res) =>{
+server.post('/cadastro', async (req, res) => {
     try {
-        const {nome_usuario, email, foto_perfil} = req.body
-        let {senha} = req.body
+        const { nome_usuario, email, foto_perfil } = req.body
+        let { senha } = req.body
 
         senha = senha.trim()
 
-        if(senha == ""){
-            return res.json({"resposta":"Preencha o campo"})
-        }else if(senha.length < 6){
-            return res.json({"resposta":"A senha deve conter no mínimo 6 caracteres"})
-        }else if(email.length < 6){
-            return res.json({"resposta":"Preencha o campo e-mail coretamente"})
-        }else if(nome_usuario.length < 6){
-            return res.send({"resposta":"Preencha o campo nome corretamente"})
+        if (senha == "") {
+            return res.json({ "resposta": "Preencha o campo" })
+        } else if (senha.length < 6) {
+            return res.json({ "resposta": "A senha deve conter no mínimo 6 caracteres" })
+        } else if (email.length < 6) {
+            return res.json({ "resposta": "Preencha o campo e-mail coretamente" })
+        } else if (nome_usuario.length < 6) {
+            return res.send({ "resposta": "Preencha o campo nome corretamente" })
         }
 
         let sql = `select * from usuarios where email = ?`
-        let [resultado_email] = await pool.query(sql,[email])
-        if(resultado_email.length != 0){
-            return res.json({"resposta":"E-mail já cadastrado"})
+        let [resultado_email] = await pool.query(sql, [email])
+        if (resultado_email.length != 0) {
+            return res.json({ "resposta": "E-mail já cadastrado" })
         }
 
         const hash = crypto.createHash("sha256").update(senha).digest("hex")
@@ -58,15 +58,16 @@ server.post('/cadastro', async (req,res) =>{
         console.log(resultado)
 
         // FUNCIONANDO AO CONTRÁRIO (VERIFICAR DEPOIS)
-        if(resultado.affectedRows > 0){
-            return res.json({"resposta":"Erro no cadastro", "res,kldmf": resultado
+        if (resultado.affectedRows > 0) {
+            return res.json({
+                "resposta": "Erro no cadastro", "res,kldmf": resultado
             })
-        }else{
-            return res.json({"resposta":"Cadastro realizado"})
+        } else {
+            return res.json({ "resposta": "Cadastro realizado" })
         }
     } catch (error) {
         console.log(error)
-    }    
+    }
 })
 
 // Atualizar nome
@@ -75,18 +76,43 @@ server.put('/atualizar_nomeUsuario', async (req, res) => {
         const { nome_usuario, email } = req.body
 
         let sql = 'SELECT * FROM usuarios WHERE email = ?'
-        let [resultado_email] = await pool.query(sql,[email])
-        if(resultado_email.length == 0){
-            return res.json({"resposta":"E-mail Inexistente"})
+        let [resultado_email] = await pool.query(sql, [email])
+        if (resultado_email.length == 0) {
+            return res.json({ "resposta": "E-mail Inexistente" })
         }
 
         sql = `UPDATE usuarios SET nome_usuario = ? WHERE email = ?`
-        
+
         const [resultado] = await pool.query(sql, [nome_usuario, email])
 
         res.json({
             "resultado": resultado,
             "mensagem": `Nome de usuário Atualizado para: ${nome_usuario}`
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+// Atualizar email
+server.put('/atualizar_emailUsuario', async (req, res) => {
+    try {
+        const { email_antigo, email_novo } = req.body
+
+        let sql = 'SELECT * FROM usuarios WHERE email = ?'
+        let [resultado_email] = await pool.query(sql, [email_antigo])
+        if (resultado_email.length == 0) {
+            return res.json({ "resposta": "E-mail Inexistente" })
+        }
+
+        sql = `UPDATE usuarios SET email = ? WHERE email = ?`
+
+        const [resultado] = await pool.query(sql, [email_novo, email_antigo])
+
+        res.json({
+            "resultado": resultado,
+            "mensagem": `Email do usuário Atualizado para: ${email_novo}`
         })
 
     } catch (error) {
