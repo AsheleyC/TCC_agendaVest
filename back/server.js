bf52d4c80dd07f45776310589305b8985b4ce334
Beatriz-Giacomini <beatrizgiacomini17@gmail.com>
2026-03-05 13:31:06 -0300

    Métodos GET e POST

--- back/server.js
+++ back/server.js
@@ -14,43 +14,57 @@ server.use(cors())
 
 server.listen(porta,()=>{  
     console.log(`Servidor rodando em: http://localhost:${porta}`)  
-}) 
-
-server.get('/', async (req,res)=>{
-    
 })
 
-//bia
-//dfjnasojfnboudanofj
+// Visualizar Perfil
+server.get('/ver_perfil', async (req,res) =>{
+    try {
+        const [resultado] = await pool.query(`select nome_usuario, email, senha, foto_perfil from usuarios`)
+        res.json({"resposta":resultado})
+    } catch (error) {
+        console.log(error)
+    }    
+})
 
+// Cadastrar Usuário
+server.post('/cadastro', async (req,res) =>{
+    try {
+        const {nome_usuario, email, foto_perfil} = req.body
+        let {senha} = req.body
 
+        senha = senha.trim()
 
+        if(senha == ""){
+            return res.json({"resposta":"Preencha o campo"})
+        }else if(senha.length < 6){
+            return res.json({"resposta":"A senha deve conter no mínimo 6 caracteres"})
+        }else if(email.length < 6){
+            return res.json({"resposta":"Preencha o campo e-mail coretamente"})
+        }else if(nome_usuario.length < 6){
+            return res.send({"resposta":"Preencha o campo nome corretamente"})
+        }
 
+        let sql = `select * from usuarios where email = ?`
+        let [resultado_email] = await pool.query(sql,[email])
+        if(resultado_email.length != 0){
+            return res.json({"resposta":"E-mail já cadastrado"})
+        }
 
+        const hash = crypto.createHash("sha256").update(senha).digest("hex")
 
+        sql = `insert into usuarios (nome_usuario, email, senha, foto_perfil) values (?, ?, ?, ?)`
+        let resultado = await pool.query(sql, [nome_usuario, email, hash, foto_perfil])
 
+        console.log(resultado)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Asheley Tombolo
+        // FUNCIONANDO AO CONTRÁRIO (VERIFICAR DEPOIS)
+        if(resultado.affectedRows >0){
+            return res.json({"resposta":"Cadastro realizado"})
+        }else{
+            return res.json({"resposta":"Erro no cadastro", "res,kldmf": resultado
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }    
+})
