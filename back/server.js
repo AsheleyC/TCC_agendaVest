4582c01e35f7ec5ca7a56a3a9961aa05473d1842
Asheley Gomes <asheleycgt@gmail.com>
2026-03-05 13:09:14 -0300

    Rotas Put e Delete
    Asheley fez as rotas PUT e DELETE do cadastro de Usuários

--- back/server.js
+++ back/server.js
@@ -1,56 +1,53 @@
-const express = require('express')  
-const server = express()  
-const cors = require('cors')  
-const mysql = require('mysql2/promise') 
-const crypto = require('crypto')  
-const pool = require('./db') 
+const express = require('express')
+const server = express()
+const cors = require('cors')
+const mysql = require('mysql2/promise')
+const crypto = require('crypto')
+const pool = require('./db')
 
-require('dotenv').config() 
+require('dotenv').config()
 
-const porta = process.env.porta 
+const porta = process.env.porta
 
-server.use(express.json())  
-server.use(cors())  
+server.use(express.json())
+server.use(cors())
 
-server.listen(porta,()=>{  
-    console.log(`Servidor rodando em: http://localhost:${porta}`)  
-}) 
-
-server.get('/', async (req,res)=>{
-    
+server.listen(porta, () => {
+    console.log(`Servidor rodando em: http://localhost:${porta}`)
 })
 
-//bia
-//dfjnasojfnboudanofj
+// Atualizar nome
+server.put('/atualizar_nomeUsuario', async (req, res) => {
+    try {
+        const { nome_usuario, email } = req.body
 
+        const sql = `UPDATE usuarios SET nome_usuario = ? WHERE email = ?`
+        const [resultado] = await pool.query(sql, [nome_usuario, email])
 
+        res.json({
+            "resultado": resultado,
+            "mensagem": `Nome de usuário Atualizado para: ${nome_usuario}`
+        })
 
+    } catch (error) {
+        console.log(error)
+    }
+})
 
+//Deletar usuário
+server.delete('/deletar_usuario', async (req, res) => {
+    try {
+        const { senha, email } = req.body
+        const sql = 'DELETE FROM usuarios WHERE email = ? AND senha = ?'
 
+        const [resultado] = await pool.query(sql, [email, senha])
 
+        res.json({
+            "resultado": resultado,
+            "mensagem": `Usuário Deletado`
+        })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Asheley Tombolo
+    } catch (error) {
+        console.log(error)
+    }
+})
