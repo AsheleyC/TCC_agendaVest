f44eede89c982e9d4d50e0984c89e9ba998c694f
Asheley Gomes <asheleycgt@gmail.com>
2026-03-17 16:06:49 -0300

    Arrumado

--- web_vest/src/app/page.js
+++ web_vest/src/app/page.js
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#e5ecf6]">
-
+    <div className="min-h-screen bg-[#e5ecf6] flex items-center justify-center text-[#4a698d]">
+      <div className="bg-[#b9d8e1] w-250 h-150" >
+        <h1>Bem-Vindo</h1>
+      </div>
 
     </div>
   );
