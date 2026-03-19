a8415adffed7e3ff53892cdbd630b0e1db5e1560
Beatriz-Giacomini <beatrizgiacomini17@gmail.com>
2026-03-19 08:27:27 -0300

    Login

---
+++ web_vest/src/app/sobre/page.js
@@ -0,0 +1,32 @@
+export default function Sobre() {
+    return (
+        <div className="min-h-screen flex flex-col bg-[#e5ecf6] text-[#4a698d] pt-15 pl-30">
+            <h1>SOBRE O AGENDAVEST</h1>
+            <h3 >Da dificuldade de encontrar qual curso escolher, qual faculdade ingressar e como estudar e se organizar nasceu o AgendaVest. </h3>
+            <div className="flex justify-center gap-5">
+                <div>
+                    <div className="bg-[#b9d8e1] w-50 h-50">
+                        <p>Mapa Interativo</p>
+                    </div>
+                    <div className="bg-[#b9d8e1] w-50 h-50">
+                        <p>Mapa Interativo</p>
+                    </div>
+                    <div className="bg-[#b9d8e1] w-50 h-50">
+                        <p>Mapa Interativo</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="bg-[#b9d8e1] w-50 h-50">
+                        <p>Mapa Interativo</p>
+                    </div>
+                    <div className="bg-[#b9d8e1] w-50 h-50">
+                        <p>Mapa Interativo</p>
+                    </div>
+                    <div className="bg-[#b9d8e1] w-50 h-50">
+                        <p>Mapa Interativo</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
