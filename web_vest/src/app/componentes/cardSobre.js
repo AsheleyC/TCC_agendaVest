905cf5b75da7d75b9a974fa9830d20a1f73c40be
Asheley Gomes <asheleycgt@gmail.com>
2026-03-19 11:07:07 -0300

    Responsivo
    Página Sobre alterada para ficar responsiva

--- web_vest/src/app/componentes/cardSobre.js
+++ web_vest/src/app/componentes/cardSobre.js
@@ -1,10 +1,13 @@
-export default function CardSobre({titulo, texto, altura}){
-    return(
-        <div className="flex flex-col p-3 bg-[#b9d8e1] w-100 h-70 text-[#4a698d] text-justify rounded-lg shadow-sm hover:shadow-md hover:scale-105 transition duration-300">
-            <h1 className="text-center text-2xl font-semibold">{titulo}</h1>
-            <p>___________________________________________</p>
-            <br></br>
-            <h3 className="text-xl">{texto}</h3>
+export default function CardSobre({ titulo, texto }) {
+    return (
+        <div className="flex flex-col p-4 bg-[#b9d8e1] w-full min-h-[220px] text-[#4a698d] text-justify rounded-lg shadow-sm hover:shadow-md hover:scale-105 transition duration-300">
+            <h1 className="text-center text-lg md:text-xl lg:text-2xl font-semibold">
+                {titulo}
+            </h1>
+            <hr className="my-2 border-[#4a698d]" />
+            <h3 className="text-sm md:text-base lg:text-lg">
+                {texto}
+            </h3>
         </div>
     )
 }
