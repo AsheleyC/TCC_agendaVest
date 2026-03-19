c03eb08539561fa3ac4b3daa9090bcf7cce46621
Beatriz-Giacomini <beatrizgiacomini17@gmail.com>
2026-03-19 11:03:03 -0300

    Update cardSobre.js

--- web_vest/src/app/componentes/cardSobre.js
+++ web_vest/src/app/componentes/cardSobre.js
@@ -1,6 +1,6 @@
 export default function CardSobre({titulo, texto, altura}){
     return(
-        <div className="flex flex-col p-3 bg-[#b9d8e1] w-100 h-70 text-[#4a698d] text-justify rounded-lg shadow-sm hover:shadow-md hover:scale-105 transition duration-300">
+        <div className="flex flex-col p-3  pt-7 bg-[#b9d8e1] w-100 h-70 text-[#4a698d] text-justify rounded-lg shadow-sm hover:shadow-md hover:scale-105 transition duration-300">
             <h1 className="text-center text-2xl font-semibold">{titulo}</h1>
             <p>___________________________________________</p>
             <br></br>
