e368a366f8b52bdf92d7cf72ed8f024f8bbfc716
Beatriz-Giacomini <beatrizgiacomini17@gmail.com>
2026-04-14 11:18:08 -0300

    Implementando o botão 'Sou Administrador'

--- web_vest/src/app/componentes/header.js
+++ web_vest/src/app/componentes/header.js
@@ -4,8 +4,8 @@ export default function Header() {
             <nav className="flex items-center justify-between px-10 py-3">
 
                 <div className="flex items-center gap-3">
-                    <img 
-                        src="https://www.designevo.com/res/templates/thumb_small/simple-black-and-white-font-style.webp" 
+                    <img
+                        src="https://www.designevo.com/res/templates/thumb_small/simple-black-and-white-font-style.webp"
                         className="w-14 h-14 rounded-full"
                     />
 
@@ -14,15 +14,15 @@ export default function Header() {
                     </h1>
                 </div>
 
-                <ul className="flex space-x-10 ">
-                    <li className="text-lg hover:underline">
+                <ul className="flex iems-center space-x-10">
+                    <li className="text-lg hover:underline leading-10">
                         <a href="/">Home</a>
                     </li>
-                    <li className="text-lg hover:underline">
+                    <li className="text-lg hover:underline leading-10">
                         <a href="/sobre">Sobre</a>
                     </li>
+                    <a href="/login" className="bg-[#629bb5] text-white px-4 py-2 rounded-3xl hover:scale-105 hover:opacity-90 transition-all duration-300">Sou Administrador</a>
                 </ul>
-
             </nav>
         </header>
     )
